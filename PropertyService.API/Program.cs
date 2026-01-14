e74f0c2655e30b2bda65bf2a2b13512f027aef6b
Mirko627 <mirkoferrarimirkoferrari@gmail.com>
2026-01-14 17:48:23 +0100

    fix problem

--- PropertyService.API/Program.cs
+++ PropertyService.API/Program.cs
@@ -85,7 +85,7 @@ var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
-    var db = scope.ServiceProvider.GetRequiredService<UserDBContext>();
+    var db = scope.ServiceProvider.GetRequiredService<PropertyDbContext>();
 
     var retries = 10;
     while (retries > 0)
